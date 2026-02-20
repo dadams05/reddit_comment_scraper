206dff56d3d02389af852e8e33e44360af90de90
David Adams <davidadams0502@gmail.com>
2026-02-19 23:08:09 -0600

    Linted and reformatted

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-import praw
+# pylint: disable=missing-module-docstring
 import os
 import time
 import json
 import argparse
 from datetime import datetime
 from dotenv import load_dotenv
-
-'''
-TODO
-1. Linter and code-formatter
-2. pyproject.toml?
-'''
+import praw
 
 # API keys
 load_dotenv()
@@ -19,19 +14,19 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 USER_AGENT = os.getenv("USER_AGENT")
 
 # globals
-OUT_DIRECTORY = "out" # directory to output files
-FILE_NAME = f"scrape_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json" # name of file to save to
-NUM_MAX_POSTS = 15 # max number of posts to scrape from for each subreddit
-NUM_COMMENTS_REPLACE = 15 # depth of comment subtrees to replace; can be None
-SUBREDDITS = [ # subreddits to scrape from
+OUT_DIRECTORY = "out"  # directory to output files
+FILE_NAME = f"scrape_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json"  # name of file to save to
+NUM_MAX_POSTS = 15  # max number of posts to scrape from for each subreddit
+NUM_COMMENTS_REPLACE = 15  # depth of comment subtrees to replace; can be None
+SUBREDDITS = [  # subreddits to scrape from
     "wallstreetbets",
     "smallstreetbets",
     "StockMarket",
     "Shortsqueeze",
     "investing",
-    "Daytrading"
+    "Daytrading",
 ]
-BLACKLIST = [ # comments containing these words/phrases will be filtered out
+BLACKLIST = [  # comments containing these words/phrases will be filtered out
     "[deleted]",
     "[removed]",
     "I am a bot",
@@ -39,13 +34,16 @@ BLACKLIST = [ # comments containing these words/phrases will be filtered out
     "![gif](giphy",
     "**User Report**",
     "I will be messaging you in",
-    "[**Join WSB Discord**]"
+    "[**Join WSB Discord**]",
 ]
 
 
 def scrape_comments():
+    """Scrapes comments from Reddit"""
     # setup
-    reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT
+    )
     comments = []
     subreddit_count = 0
     post_count = 0
@@ -63,34 +61,37 @@ def scrape_comments():
             post_count += 1
             submission.comment_sort = "top"
             submission.comments.replace_more(limit=NUM_COMMENTS_REPLACE)
-            time.sleep(1) # to avoid rate limiting
+            time.sleep(1)  # to avoid rate limiting
             # store the comment data
             for comment in submission.comments.list():
                 scraped_comment_count += 1
                 comment_data = {}
                 comment_data["id"] = comment.id
-                comment_data["created"] = str(datetime.fromtimestamp(submission.created_utc))
+                comment_data["created"] = str(
+                    datetime.fromtimestamp(submission.created_utc)
+                )
                 comment_data["score"] = comment.score
                 comment_data["text"] = comment.body
                 comments.append(comment_data)
-    
+
     # filter out comments with blacklisted words (bot comments)
     print(">> Filtering")
     filtered = []
     for comment in comments:
-        if (any(keyword in comment["text"] for keyword in BLACKLIST)): continue
+        if any(keyword in comment["text"] for keyword in BLACKLIST):
+            continue
         sanitized = comment["text"].replace("\n", " ")
         sanitized = " ".join(sanitized.split())
         comment["text"] = sanitized
         filtered.append(comment)
         filtered_comment_count += 1
     comments = filtered
-    
+
     # save the comments to a file
     os.makedirs(OUT_DIRECTORY, exist_ok=True)
     with open(os.path.join(OUT_DIRECTORY, FILE_NAME), "w", encoding="utf-8") as file:
         json.dump(comments, file, indent=4, ensure_ascii=False)
-    
+
     # wrap up and print metrics
     end_time = time.perf_counter()
     print(">> Finished")
@@ -102,25 +103,32 @@ def scrape_comments():
 
 
 def extract_from_scrape(in_file_name: str, out_file_name="extracted.txt"):
+    """Given a scraped .json file, will extract only the comments to a .txt file"""
     comments = []
     with open(in_file_name, "r", encoding="utf-8") as file:
         data = json.load(file)
         for comment in data:
             comments.append(comment["text"])
-    
-    with open(os.path.join(OUT_DIRECTORY, out_file_name), "w", encoding="utf-8") as file:
+
+    with open(
+        os.path.join(OUT_DIRECTORY, out_file_name), "w", encoding="utf-8"
+    ) as file:
         for c in comments:
             file.write(c + "\n")
 
 
 if __name__ == "__main__":
     # set up argument parser
-    # only argument is extract, which will extract the comments from a .json file and put them in a .txt file
     parser = argparse.ArgumentParser(
         prog="Reddit Comment Scraper",
-        description="Program to scrape comments from subreddit posts"
+        description="Program to scrape comments from subreddit posts",
     )
-    parser.add_argument("-e", "--extract", type=str, help="scraped .json file to extract comments text from")
+    parser.add_argument(
+        "-e",
+        "--extract",
+        type=str,
+        help="scraped .json file to extract comments text from",
+    )
     args = parser.parse_args()
 
     if args.extract:
